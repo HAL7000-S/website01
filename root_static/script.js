89cb27da98e2bd83ed3067e73007bd2736a40da2
HAL_2 <halki_ms_2@outlook.jp>
2023-12-17 23:45:53 +0900

    大幅改変

---
+++ root_static/script.js
@@ -0,0 +1,31 @@
+let nameButton = document.querySelector("button");
+let yourHeaddingName = document.querySelector(".yourName");
+let age_value = document.querySelector(".age");
+let age;
+
+// window.onloadは、ウェブページが完全に読み込まれたときに実行される関数を設定するための方法です。
+// 年齢更新関数
+window.onload = function () {
+    var today = new Date();
+    age_value.textContent = today.getFullYear() - 2003;
+    }
+
+
+// 相手の名前代入
+if (!localStorage.getItem("name")) {
+    setUserName();
+} else {
+    const storedName = localStorage.getItem("name");
+    yourHeaddingName.textContent = `また会いましたね。${storedName}`;
+}
+
+// クリック時の変更
+function setUserName() {
+    const yourName = prompt("あなたの名前を入力してください。");
+    localStorage.setItem("name", yourName);
+    yourHeaddingName.textContent = `初めまして、${yourName}!`;
+    }
+// クリック時の名前変更
+nameButton.onclick = () => {
+    setUserName();
+  };
