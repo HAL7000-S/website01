9e38273e8ac73327ce2014891c032138d32c4a0f
HAL_2 <halki_ms_2@outlook.jp>
2024-05-25 14:58:49 +0900

    complete_asoci

--- asociatron/static/asoci.js
+++ asociatron/static/asoci.js
@@ -1,18 +1,33 @@
-
+var inputVector = [0,0,0]
 var memoryMatrix = [
     [0, 0, 0],
     [0, 0, 0],
     [0, 0, 0]
 ];
 
+var counter = 0;
 
-function updateMatrix() {
-    const x1 = parseFloat(document.getElementById('input1').value) || 0;
-    const x2 = parseFloat(document.getElementById('input2').value) || 0;
-    const x3 = parseFloat(document.getElementById('input3').value) || 0;
 
-    const x11 = x1 * x1, x22 = x2 * x2, x33 = x3 * x3;
-    const x12 = x1 * x2, x23 = x2 * x3, x31 = x3 * x1;
+function updateInputMatrix() {
+
+    const inputVectorTex = `
+    \\boldsymbol{x}
+    = \\begin{pmatrix}
+    x_1 \\\\
+    x_2 \\\\
+    x_3
+    \\end{pmatrix}
+    = \\begin{pmatrix}
+    ${inputVector[0]} \\\\
+    ${inputVector[1]} \\\\
+    ${inputVector[2]}
+    \\end{pmatrix}`;
+    document.getElementById('inputVector').innerHTML = `$$ ${inputVectorTex} $$`;
+    MathJax.typesetPromise([document.getElementById('inputVector')]);
+
+
+    const x11 = inputVector[0] * inputVector[0], x22 = inputVector[1] * inputVector[1], x33 = inputVector[2] * inputVector[2];
+    const x12 = inputVector[0] * inputVector[1], x23 = inputVector[1] * inputVector[2], x31 = inputVector[2] * inputVector[0];
 
     const inputMatrixTex = `X
         = \\boldsymbol{x}\\boldsymbol{x}^T
@@ -29,21 +44,27 @@ function updateMatrix() {
 
     document.getElementById('inputMatrix').innerHTML = `$$ ${inputMatrixTex} $$`;
     MathJax.typesetPromise([document.getElementById('inputMatrix')]);
+}
 
+function updateMemoryOutput() {
     // memoryMatrixの更新
-    memoryMatrix[0][0] += x11;
+    const x11 = inputVector[0] * inputVector[0], x22 = inputVector[1] * inputVector[1], x33 = inputVector[2] * inputVector[2];
+    const x12 = inputVector[0] * inputVector[1], x23 = inputVector[1] * inputVector[2], x31 = inputVector[2] * inputVector[0];
+    // memoryMatrix[0][0] += x11;
     memoryMatrix[0][1] += x12;
     memoryMatrix[0][2] += x31;
     memoryMatrix[1][0] += x12;
-    memoryMatrix[1][1] += x22;
+    // memoryMatrix[1][1] += x22;
     memoryMatrix[1][2] += x23;
     memoryMatrix[2][0] += x31;
     memoryMatrix[2][1] += x23;
-    memoryMatrix[2][2] += x33;
+    // memoryMatrix[2][2] += x33;
+
+    counter += 1;
 
     const memoryMatrixTex = `
     M
-    = \\sum_t X_t
+    = \\sum_{t=1}^${counter} X_t - ${counter}I
     = \\begin{pmatrix}
     ${memoryMatrix[0][0]} & ${memoryMatrix[0][1]} & ${memoryMatrix[0][2]} \\\\
     ${memoryMatrix[1][0]} & ${memoryMatrix[1][1]} & ${memoryMatrix[1][2]} \\\\
@@ -54,30 +75,122 @@ function updateMatrix() {
     document.getElementById('memoryMatrix').innerHTML = `$$ ${memoryMatrixTex} $$`;
     MathJax.typesetPromise([document.getElementById('memoryMatrix')]);
 
-    const y1 = phi(memoryMatrix[0][0])*x1 + phi(memoryMatrix[0][1])*x2 + phi(memoryMatrix[0][2])*x3;
-    const y2 = phi(memoryMatrix[1][0])*x1 + phi(memoryMatrix[1][1])*x2 + phi(memoryMatrix[1][2])*x3;
-    const y3 = phi(memoryMatrix[2][0])*x1 + phi(memoryMatrix[2][1])*x2 + phi(memoryMatrix[2][2])*x3;
+    const y1 = phi(phi(memoryMatrix[0][0])*inputVector[0] + phi(memoryMatrix[0][1])*inputVector[1] + phi(memoryMatrix[0][2])*inputVector[2]);
+    const y2 = phi(phi(memoryMatrix[1][0])*inputVector[0] + phi(memoryMatrix[1][1])*inputVector[1] + phi(memoryMatrix[1][2])*inputVector[2]);
+    const y3 = phi(phi(memoryMatrix[2][0])*inputVector[0] + phi(memoryMatrix[2][1])*inputVector[1] + phi(memoryMatrix[2][2])*inputVector[2]);
     
     const outputVectorTex = `
     \\boldsymbol{y} 
     = \\phi(\\phi(M)\\boldsymbol{x})
-    =\\begin{pmatrix}
-    ${phi(y1)} \\\\
-    ${phi(y2)} \\\\
-    ${phi(y3)}
+    = \\phi(\\begin{pmatrix}
+    ${phi(memoryMatrix[0][0])} & ${phi(memoryMatrix[0][1])} & ${phi(memoryMatrix[0][2])} \\\\
+    ${phi(memoryMatrix[1][0])} & ${phi(memoryMatrix[1][1])} & ${phi(memoryMatrix[1][2])} \\\\
+    ${phi(memoryMatrix[2][0])} & ${phi(memoryMatrix[2][1])} & ${phi(memoryMatrix[2][2])}
     \\end{pmatrix}
-`;
+    \\begin{pmatrix}
+    ${inputVector[0]} \\\\
+    ${inputVector[1]} \\\\
+    ${inputVector[2]}
+    \\end{pmatrix}
+    )
+    =\\begin{pmatrix}
+    ${y1} \\\\
+    ${y2} \\\\
+    ${y3}
+    \\end{pmatrix}`;
 
     document.getElementById('outputVector').innerHTML = `$$ ${outputVectorTex} $$`;
     MathJax.typesetPromise([document.getElementById('outputVector')]);
+
+
+    // ラインの色と太さを変更
+    var line = document.getElementById("arrow12");
+    line.setAttribute('style', getLineStyle(memoryMatrix[0][1]));
+    var line = document.getElementById(`arrow23`);
+    line.setAttribute('style', getLineStyle(memoryMatrix[1][2]));
+    var line = document.getElementById(`arrow31`);
+    line.setAttribute('style', getLineStyle(memoryMatrix[2][0]));
+
+    var element = document.getElementById('text12');
+    element.textContent = memoryMatrix[0][1].toString();
+    var element = document.getElementById('text23');
+    element.textContent = memoryMatrix[1][2].toString();
+    var element = document.getElementById('text31');
+    element.textContent = memoryMatrix[2][0].toString();
+
+    outputChangeColor([y1, y2, y3]);
 }
 
 function phi(value) {
-    if (value > 1) {
+    if (value >= 1) {
         return 1;
-    } else if (value < 1) {
+    } else if (value <= -1) {
         return -1;
     } else {
         return 0;
     }
+}
+
+
+// ノードのをクリックしたときの関数
+function changeColor(element, index) { 
+    if (element.classList.contains("highlighted-akane")) {
+        element.classList.remove("highlighted-akane");
+        element.classList.add("highlighted-aoi");
+        inputVector[index] = -1;
+    } else if (element.classList.contains("highlighted-aoi")) {
+        element.classList.remove("highlighted-aoi");
+        inputVector[index] = 0;
+    } else { // class=="node"
+        element.classList.add("highlighted-akane");
+        inputVector[index] = 1;
+    }
+    updateInputMatrix();
+}
+
+// 出力するときのノードの色変更関数
+function outputChangeColor(y_list){
+    for (index = 1; index <= 3; index++){
+        element = document.getElementById(`node${index}`)
+        if (element.classList.contains("highlighted-akane")) {
+            element.classList.remove("highlighted-akane");
+        } else if (element.classList.contains("highlighted-aoi")) {
+            element.classList.remove("highlighted-aoi");
+        }
+
+        if (y_list[index-1] == 1) {
+            element.classList.add("highlighted-akane");
+        } else if (y_list[index-1] == -1){
+            element.classList.add("highlighted-aoi");
+        }
+        inputVector[index-1] = y_list[index-1]
+    }
+
+    updateInputMatrix();
+}
+
+
+document.getElementById("node1").addEventListener("click", function() {
+    changeColor(this, 0);
+});
+document.getElementById("node2").addEventListener("click", function() {
+    changeColor(this, 1);
+});
+document.getElementById("node3").addEventListener("click", function() {
+    changeColor(this, 2);
+});
+
+
+// ラインの色と太さを決定する関数
+function getLineStyle(value) {
+    width = Math.min(20, Math.max(1, Math.abs(value*2)));
+    if (value == 0){
+        colorName = "black";
+    } else if (value >= 1) {
+        colorName = "red";
+    } else if (value <= -1) {
+        colorName = "blue";
+    }
+
+    return `stroke-width: ${width}; stroke: ${colorName};`;
 }
